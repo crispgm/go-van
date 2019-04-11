6400ecc15d706e10403e4f3a9bee2c549d0b12d4
David Zhang <crispgm@gmail.com>
2019-04-11 14:54:03 +0800

    Test PrintLog

--- caravan/print_test.go
+++ caravan/print_test.go
@@ -6,6 +6,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestPrintLog(t *testing.T) {
+	output := CaptureOutput(func() { PrintLog("test log") })
+	assert.Regexp(t, `^\[[0-9]{2}:[0-9]{2}:[0-9]{2}\] test log\n$`, output)
+}
+
 func TestWarningSound(t *testing.T) {
 	output := CaptureOutput(func() { WarningSound() })
 	assert.Equal(t, "\a", output)
