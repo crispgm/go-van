d0c7284109fd7f57a247d3110874ca50e2306d0d
David Zhang <crispgm@gmail.com>
2019-04-11 14:17:54 +0800

    Test rsync with args

--- deploy/rsync_test.go
+++ deploy/rsync_test.go
@@ -1,6 +1,7 @@
 package deploy
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,6 +9,21 @@ import (
 
 func TestRsyncDeploy(t *testing.T) {
 	var rsync RSync
+	os.Remove("../fixtures/testoutput/file.txt")
+	os.Remove("../fixtures/testoutput/file_excluded.txt")
 	rsync.Run("../fixtures/testsrc/file.txt", "../fixtures/testoutput", nil)
 	assert.FileExists(t, "../fixtures/testoutput/file.txt")
+	os.Remove("../fixtures/testoutput/file.txt")
+	os.Remove("../fixtures/testoutput/file_excluded.txt")
+}
+
+func TestRsyncDeployWithArgs(t *testing.T) {
+	var rsync RSync
+	os.Remove("../fixtures/testoutput/file.txt")
+	os.Remove("../fixtures/testoutput/file_excluded.txt")
+	rsync.Run("../fixtures/testsrc/file.txt", "../fixtures/testoutput", []string{"--exclude=file_excluded.txt"})
+	assert.FileExists(t, "../fixtures/testoutput/file.txt")
+	_, err := os.Stat("../fixtures/testoutput/file_excluded.txt")
+	assert.True(t, os.IsNotExist(err))
+	os.Remove("../fixtures/testoutput/file.txt")
 }
