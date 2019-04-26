e9b6834406b40dd01d2b6b41dfcc6b2d606b283c
David Zhang <crispgm@gmail.com>
2019-04-26 17:39:00 +0800

    Test more on events

--- caravan/event_ctrl_test.go
+++ caravan/event_ctrl_test.go
@@ -10,14 +10,23 @@ import (
 
 func TestFireEvent(t *testing.T) {
 	conf := &caravan.Conf{
-		Debug:  true,
-		OnInit: []string{"ls", "echo \"i love van\""},
+		Debug:    true,
+		OnInit:   []string{"ls", "echo \"i love van\""},
+		OnChange: []string{"echo changed"},
+		OnError:  []string{"echo error"},
+		OnDeploy: []string{"echo deployed"},
 	}
 	output := caravan.CaptureOutput(func() {
 		eventCtrl := caravan.NewEventCtrl(conf)
 		eventCtrl.EventLoop()
 		eventCtrl.FireEvent(caravan.NewEmptyEvent(caravan.HookOnInit))
+		eventCtrl.FireEvent(caravan.NewEmptyEvent(caravan.HookOnChange))
+		eventCtrl.FireEvent(caravan.NewEmptyEvent(caravan.HookOnDeploy))
+		eventCtrl.FireEvent(caravan.NewEmptyEvent(caravan.HookOnError))
 		time.Sleep(1000 * time.Millisecond)
 	})
 	assert.Contains(t, output, "SYSTEM Handling event: OnInit on")
+	assert.Contains(t, output, "SYSTEM Handling event: OnChange on")
+	assert.Contains(t, output, "SYSTEM Handling event: OnDeploy on")
+	assert.Contains(t, output, "SYSTEM Handling event: OnError on")
 }
--- caravan/event_test.go
+++ caravan/event_test.go
@@ -10,3 +10,16 @@ func TestNewEventWithType(t *testing.T) {
 	e := NewEmptyEvent(HookOnError)
 	assert.Equal(t, e.EventType, HookOnError)
 }
+
+func TestNewEvent(t *testing.T) {
+	e := NewEvent(HookOnChange, "", "/path/to/some/place", GetFileName("/path/to/some/place"))
+	assert.Equal(t, HookOnChange, e.EventType)
+	assert.Equal(t, "", e.Event)
+	assert.Equal(t, "/path/to/some/place", e.Path)
+	assert.Equal(t, "place", e.Filename)
+}
+
+func TestRunCommands(t *testing.T) {
+	_, err := runCommands([]string{})
+	assert.EqualError(t, ErrNoCommand, err.Error())
+}
