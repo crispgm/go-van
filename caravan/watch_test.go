a9f95dff083f85bf42e8d05b9c86762e2c35c4a5
David Zhang <crispgm@gmail.com>
2019-05-06 14:17:55 +0800

    Test watch() with LogFormat in conf

--- caravan/watch_test.go
+++ caravan/watch_test.go
@@ -28,6 +28,25 @@ func TestWatchFile(t *testing.T) {
 	<-done
 }
 
+func TestWatchFileWithLogFmt(t *testing.T) {
+	conf := Conf{
+		Source:      "../fixtures/testsrc",
+		Destination: "../fixtures/testoutput",
+		LogFormat:   "[%t] Event <%e> %p",
+	}
+	done := make(chan int)
+	go Watch(conf, func(ei notify.EventInfo) error {
+		defer close(done)
+		assert.Contains(t, ei.Path(), "fixtures/testsrc/created_by_watch.test")
+		return errTestBreak
+	})
+	time.Sleep(100 * time.Millisecond)
+	fn := fmt.Sprintf("%s/created_by_watch.test", conf.Source)
+	os.Remove(fn)
+	os.Create(fn)
+	<-done
+}
+
 func TestIsDir(t *testing.T) {
 	assert.True(t, isDir("."))
 	assert.False(t, isDir("./aaa"))
