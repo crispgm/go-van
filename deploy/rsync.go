44b772b9dfcc8a8da84d291798041d09df7fefb7
David Zhang <crispgm@gmail.com>
2020-01-13 16:36:27 +0800

    Chore: remove trailing line

--- deploy/rsync.go
+++ deploy/rsync.go
@@ -12,7 +12,6 @@ func (r RSync) Run(src, dst string, extraArgs []string) ([]byte, error) {
 	args := []string{"-avl", src, dst}
 	if extraArgs != nil && len(extraArgs) > 0 {
 		args = append(args, extraArgs...)
-
 	}
 	cmd := exec.Command("rsync", args...)
 	output, err := cmd.Output()
